f238c10a5d4c5b957ba7004948acf2ffa1ef080b
zarnasavaliya09 <113407633+zarnasavaliya09@users.noreply.github.com>
2023-02-16 12:47:43 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcBalloon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
